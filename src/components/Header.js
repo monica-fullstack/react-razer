295385488941411668ae5d2b18ba70e864f54826
monica <monica@fullstack.ph>
2020-03-31 15:20:52 +0800

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container, Nav, NavItem, NavLink } from 'reactstrap';
+
+const Header = () => {
+  return (
+    <header >
+      <Nav className="navbar navbar-expand-lg navbar-light bg-dark">
+        <a href="#" className="navbar-brand">Razer</a>
+      </Nav>
+    </header>
+  )
+};
+
+export default Header;
