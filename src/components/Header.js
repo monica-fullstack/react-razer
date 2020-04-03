c56738a5d911407821b1b2a5c28c316ba3cecede
monica <monica@fullstack.ph>
2020-04-03 17:59:21 +0800

    added header menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,31 @@ const Header = () => {
   return (
     <header >
       <Nav className="navbar navbar-expand-lg navbar-light bg-dark">
-        <a href="#" className="navbar-brand">
-          <img src={require('../assets/images/razer-nari-logo.svg')} />
-        </a>
+        <Container>
+          <a href="#" className="navbar-brand">
+            <img src={require('../assets/images/logo.png')} />
+          </a>
+
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a className="nav-link">Products</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link">About</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link">Community</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link">Contact Us</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link">Buy Now</a>
+              </li>
+            </ul>
+          </div>
+        </Container>
       </Nav>
     </header>
   )
